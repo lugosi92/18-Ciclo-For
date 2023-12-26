3cb367415047103e41da26f05c36bbecebe209a6
khaled___beno <khaled___beno@localhost>
2023-12-26 19:37:39 +0100

    For

---
+++ 18_Ciclo_For/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+				/*ciclos while, do-while,for, foreach
+				
+				int num = 0;
+				
+				while (num < 10) {
+					System.out.println("Hola mundo");
+					num++;
+				}
+			}
+				
+				//Caracteristicas principales de los ciclos
+				// FINIDUD -> finalizar
+				
+				int num  =0;
+				do{
+					System.out.println("Hola mundo");
+					num++;
+				}while( num < 10);
+			}
+			
+		}
+
+	}*/
+
+ 
+		//for(condicion de entrada ; condicionSalida ; incrementoDecremento)
+		for( int num = 10 ; num <10 ; num++) {
+			System.out.println("El ciclo for se repite 10 veces");
+			
+		}
+	}
+}
